25133e9016db35023b899a75261a362d68b3752c
Veleoran <erwan.mazeas.14.17@gmail.com>
2024-01-16 14:56:09 +0100

    Ajout  du composant Header

---
+++ games/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function Header() {
+  return (
+    <header>
+
+    </header>
+  );
+}
+
+export default Header;
