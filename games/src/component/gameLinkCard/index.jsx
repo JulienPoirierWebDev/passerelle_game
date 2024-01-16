80f57333a9d3aaa70d154593f4ca94f79a5daacd
Jason Saint-Germain <jasonsaintgermain@gmail.com>
2024-01-16 16:04:33 +0100

    Ajout de la homepage + GameLinkCard

---
+++ games/src/component/gameLinkCard/index.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+const GameLinkCard = () => {
+    const paths = [
+        {
+          path: "/",
+          name: "Accueil",
+        },
+      ];
+
+      return (
+        <>
+          <div>
+            <ul className="hidden md:flex flex-row gap-6 justify-center align-center">
+              {paths.map((element) => {
+                return (
+                  <Link to={element.path} key={element.path}>
+                    {element.name}
+                    </Link>
+                );})}
+            </ul>
+        </div>
+        </>)
+}
+export default GameLinkCard;
