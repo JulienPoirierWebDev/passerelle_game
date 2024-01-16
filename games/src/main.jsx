8cab0e845eb215101f5cc986cc18dcd2fb5c7d24
Cisco29 <castro.francisco@live.fr>
2024-01-16 12:56:19 +0100

    mise ne place router
    router

--- games/src/main.jsx
+++ games/src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
+
+
+const router = createBrowserRouter([
+  {  
+  // errorElement: <ErrorPage />,
+  children: [
+    { path: "/", element: <App /> },
+    // { path: "/nos-produits", element: <ProductsPage /> },
+    // { path: "/produit/:id", element: <ProductPage /> },
+    // { path: "/panier", element: <CartPage /> },
+    // { path: "/connexion", element: <LoginPage /> },
+    // { path: "/inscription", element: <RegisterPage /> },
+    // { path: "/mon-compte", element: <AccountPage /> },
+  ],
+    
+    
+
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router = {router}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </RouterProvider>
 )
+
+
+
