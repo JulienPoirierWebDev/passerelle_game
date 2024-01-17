5778bd6ad78436fe7fa3e522b198668b99c5372d
JulienPoirierWebDev <connexion@julienpoirier-Webdev.com>
2024-01-17 14:47:20 +0100

    fix

--- games/src/components/gameSimon/GameSimon.jsx
+++ games/src/components/gameSimon/GameSimon.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import GameBtn from "../gameBtn/GameBtn";
 
 // Définition des couleurs disponibles pour le jeu
