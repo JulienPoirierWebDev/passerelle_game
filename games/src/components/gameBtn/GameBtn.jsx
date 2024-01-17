5d11f4fa509b9fc6677bc951f5718e75ed4a82d1
Cisco29 <castro.francisco@live.fr>
2024-01-17 11:53:13 +0100

    ajout des 3 autres btn

--- games/src/components/gameBtn/GameBtn.jsx
+++ games/src/components/gameBtn/GameBtn.jsx
@@ -1,10 +1,10 @@
 
 
-const GameBtn = (({  border, bg, type = "button", /*onClick*/ }) => (
+const GameBtn = (({  border, bg, type = "button", onClick }) => (
   <button
-    
+    onClick = {console.log (bg)}
     className={` w-[175px] sm:w-[200px] h-[175px] sm:h-[200px] m-2 duration-200 hover:scale-105  ${border} ${bg} ` }
-    //onClick={onClick}
+    // onClick={onClick}
     type = {type}
   ></button>
 ));
--- games/src/components/gameSimon/GameSimon.jsx
+++ games/src/components/gameSimon/GameSimon.jsx
@@ -9,9 +9,25 @@ function GameSimon() {
           <GameBtn
             bg="bg-blue-500"
             border="border-solid border-2 border-indigo-600"
-          />
+
+            /> 
+         
+           <GameBtn
+            bg="bg-green-500"
+            border="border-solid border-2 border-indigo-600"
+            />
+
+            <GameBtn
+            bg="bg-yellow-500"
+            border="border-solid border-2 border-indigo-600"
+            />
+             <GameBtn
+            bg="bg-red-500"
+            border="border-solid border-2 border-indigo-600"
+            />
         </div>
       </div>
+      
     </>
   );
 }
