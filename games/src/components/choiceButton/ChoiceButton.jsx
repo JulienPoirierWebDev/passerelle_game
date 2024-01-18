fadbb3c64591f2000ad87b41f18c0ca1b46c0e20
Fredericvamer <frederic.vamer@epsi.fr>
2024-01-18 10:45:50 +0100

    visuel_V2_Chifumi

--- games/src/components/choiceButton/ChoiceButton.jsx
+++ games/src/components/choiceButton/ChoiceButton.jsx
@@ -1,4 +1,4 @@
-function ChoiceButton({children, bgColor, onClick}) {
+function ChoiceButton({children, bgColor, onClick, customStyle}) {
   return (
     <button className={`${bgColor} text-white  font-bold py-2 px-4 rounded`} type="button" onClick={onClick}>{children}</button>
   )
