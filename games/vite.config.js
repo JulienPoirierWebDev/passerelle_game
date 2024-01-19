6c52252ca215ac81f2886fac5c1b70a41d38a500
Veleoran <erwan.mazeas.14.17@gmail.com>
2024-01-19 09:47:14 +0100

    test vitest
    json vitest

--- games/vite.config.js
+++ games/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), viteCommonjs()],
+  test: {
+    // Ajoutez ici vos options de configuration pour Vitest si nécessaire.
+  },
+});
